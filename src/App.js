c95178904066ae9adb6af73ba10e9ef21be38ada
Obhi, Jagpreet <jagpreet_obhi@manulife.com>
2017-12-28 13:17:06 -0500

    refinement

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React, {Component} from 'react';
 import './App.css'; // Empty
-import {Grid, Navbar, Jumbotron, Button} from 'react-bootstrap';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">REACT (with Bootstrap)</a>
-              </Navbar.Brand>
-            </Navbar.Header>
-            <Button
-              className="pull right"
-              bsStyle="primary"
-              bsSize="xsmall"
-              href="http://react-bootstrap.github.io/components.html"
-              target="_blank">
-              View React Bootstrap Docs
-            </Button>
-          </Grid>
-        </Navbar>
-        <Jumbotron></Jumbotron>
-      </div>
-    );
+    return ( <div><Header/></div>);
   }
 }
 
