b420bf92e6139f99401f2cf39ffa6ebd2fd7ff29
Obhi, Jagpreet <jagpreet_obhi@manulife.com>
2017-12-31 15:56:15 -0500

    redux store in 20 lines

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import './App.css'; // Empty
 import Header from './components/Header';
+import Source from './components/Source';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
-    return ( <div><Header/></div>);
+    return (
+      <div>
+        <Header/>
+        <Source/>
+        <Counter/>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,66 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const myReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+// Redux Store
+const createStore = (reducer) => {
+
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+    const dispatch = (action) => {
+        // new state (call reducer to determine the new state on the basis of action)
+        state = reducer(state, action);
+        listeners.forEach(listener => {
+            listener();
+        });
+    };
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => listeners.filter(l => l !== listener);
+    };
+
+    dispatch({});
+
+    return {getState, dispatch, subscribe};
+};
+
+const store = createStore(myReducer);
+
+const DisplayCounter = ({value, onIncrement, onDecrement}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
+const Counter = () => {
+    return <DisplayCounter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+        onDecrement={() => store.dispatch({type: 'DECREMENT'})}/>
+};
+
+const render = () => {
+    ReactDOM.render(
+        <Counter/>, document.getElementById('root'))
+};
+
+store.subscribe(render);
+
+render();
 
 registerServiceWorker();
